60d68631346718d0f57c6f5c781e81405a4cafaa
swalla27 <swalla27@asu.edu>
2026-03-03 16:00:27 -0600

    lab3 bt receiver design

--- labs/lab3/swalla27_lab3.py
+++ labs/lab3/swalla27_lab3.py
@@ -18,62 +18,80 @@ class Tone():
     def __init__(self, freq: float, pwr: float, snr: float):
         self.init_freq = freq
         self.init_pwr = pwr
-        self.init_snr = snr
         self.freq = freq
         self.pwr = pwr
-        self.snr = snr
+        self.noise_pwr = pwr - snr
 
 desired = Tone(2402e6, -30, 0)
 blocker = Tone(2403e6, -20, 0)
 signals = [desired, blocker]
 
-def bandpass(sig, f_center=2402e6, bandwidth=40e6, atten_pass=0, atten_stop=35, nf=2):
-    sig.snr -= nf
-    if (f_center - bandwidth/2) < sig.freq < (f_center + bandwidth/2):
-        sig.pwr -= atten_pass
-        return
-    else:
-        sig.pwr -= atten_stop
-        return
+def bandpass(signals, f_center: float, bandwidth: float, atten_pass: float, atten_stop: float, nf: float):
+    for sig in signals:
+        sig.snr -= nf
+        if (f_center - bandwidth/2) < sig.freq < (f_center + bandwidth/2):
+            sig.pwr -= atten_pass
+        else:
+            sig.pwr -= atten_stop
+    return
     
-def lna(sig, gain=15, nf=2):
-    sig.pwr += gain
-    sig.snr -= nf
+def amplifier(signals, gain: float, nf: float):
+    for sig in signals:
+        sig.pwr += gain
+        sig.snr -= nf
     return
 
-def mixer(sig, f_osc=2412e6, gain=0, nf=2):
-    sig.freq = abs(sig.freq - f_osc)
-    sig.pwr += gain
-    sig.snr -= nf
+def mixer(signals, f_osc: float, gain: float, nf: float):
+    x = list()
+    for sig in signals:
+        sig.freq = abs(sig.freq - f_osc)
+        sig.pwr += gain
+        sig.snr -= nf
+        x.append(sig)
+        x.append(Tone(freq=sig.freq+f_osc, pwr=sig.pwr+gain, snr=sig.snr-nf))
+    return x
+
+def lowpass(signals, f_corner: float, atten_pass: float, atten_stop: float, nf: float):
+    for sig in signals:
+        sig.snr -= nf
+        if sig.freq <= f_corner:
+            sig.pwr -= atten_pass
+        else:
+            sig.pwr -= atten_stop
     return
 
-def lowpass(sig, f_corner=15e6, atten_pass=0, atten_stop=50, nf=2):
-    sig.snr -= nf
-    if sig.freq <= f_corner:
-        sig.pwr -= atten_pass
-        return
-    else:
-        sig.pwr -= atten_stop
-        return
+def create_graph(signals: list, name: str):
+    for sig in signals:
+        plt.scatter(sig.freq/1e6, sig.pwr)
+    plt.xlabel('Frequency (MHz)')
+    plt.ylabel('Power (dBm)')
+    plt.title(f'Frequency Spectrum ({name})')
+    plt.grid(True)
+    # plt.show()
 
-def if_amp(sig, gain=60, nf=2):
-    sig.pwr += gain
-    sig.snr -= nf
-    return
+bandpass(signals, f_center=2402e6, bandwidth=40e6, atten_pass=0, atten_stop=35, nf=2)
+# create_graph(signals, name='A')
 
-for signal in signals:
-    bandpass(signal)
-    lna(signal)
-    mixer(signal)
-    lowpass(signal)
-    if_amp(signal)
-    bandpass(signal, f_center=10e6, bandwidth=1e6)
+amplifier(signals, gain=15, nf=2)
+# create_graph(signals, name='B')
+
+signals = mixer(signals, f_osc=2412e6, gain=0, nf=2)
+# create_graph(signals, name='C')
+
+lowpass(signals, f_corner=15e6, atten_pass=0, atten_stop=50, nf=2)
+# create_graph(signals, name='D')
+
+amplifier(signals, gain=60, nf=10)
+# create_graph(signals, name='E')
+
+bandpass(signals, f_center=10e6, bandwidth=1e6, atten_pass=0, atten_stop=35, nf=2)
+# create_graph(signals, name='F')
+
 
 print(f'Desired Signal:')
 print(f'\t{desired.init_freq/1e6:.0f} MHz -> {desired.freq/1e6:.0f} MHz')
 print(f'\t{desired.init_pwr} dBm -> {desired.pwr} dBm')
 # print(f'\t{desired.init_snr} dB -> {desired.snr} dB')
-
 
 print(f'Blocker Signal:')
 print(f'\t{blocker.init_freq/1e6:.0f} MHz -> {blocker.freq/1e6:.0f} MHz')
