1cb4e44d348885fdb1ade3d5aa964f1bed74a6dd
swalla27 <swalla27@asu.edu>
2026-03-06 09:21:18 -0600

    makes tables now

--- labs/lab3/component_class.py
+++ labs/lab3/component_class.py
@@ -13,42 +13,26 @@ import time
 import sys
 import os
 
+sys.path.append('/home/steven-wallace/Documents/asu/eee-598')
+from unit_conv_func import *
+
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ["Nimbus Roman"],
+    "mathtext.fontset": "cm"
+})
+
 #################################
 ##### Functions and Classes #####
 #################################
 
-def dB_to_rat(dB: float):
-    """A function which converts a dB value to a ratio."""
-
-    return 10**(dB/10)
-
-def rat_to_dB(rat: float):
-    """A function which converts a ratio to dB."""
-
-    return 10*np.log10(rat)
-
-def dBm_to_Vpk(power_dBm: float):
-    """A function which converts a power in dBm to a voltage Vpk, assuming the impedance seen is 50 ohms."""
-
-    power_watts = 1e-3 * (10**(power_dBm/10))
-    Vpk = np.sqrt(power_watts*2*50)
-    return Vpk
-
-def Vpk_to_dBm(Vpk: float):
-    """A function which converts a voltage to a power in dBm. This is used to convert the FFT into dBm."""
-
-    Vrms = Vpk / np.sqrt(2)
-    power_watts = Vrms**2 / 50
-    power_dBm = 10 * np.log10(power_watts / 1e-3)
-    return power_dBm
-
 def display_voltage(voltage: float):
     if voltage < 1e-3:
-        return f'{voltage*1e6:.2f} uV'
+        return f'{voltage*1e6:.1f} uV'
     elif voltage < 1:
-        return f'{voltage*1e3:.2f} mV'
+        return f'{voltage*1e3:.1f} mV'
     else:
-        return f'{voltage:.2f} V' 
+        return f'{voltage:.1f} V' 
 
 class Component():
     def __init__(self, name: str, gain_dB: float, nfig: float, iip3_dBm: float, cpout: float):
@@ -71,6 +55,8 @@ PIN = -70 # Input power in dBm.
 POUT_WANTED = -33.8 # Desired output power in dBm.
 BANDWIDTH = 1e6 # The bandwidth in Hz.
 pnoise_in = -174 + 10*np.log10(BANDWIDTH) # The noise power at the input in dBm.
+
+TABLE_DIR = '/home/steven-wallace/Documents/asu/eee-598/labs/lab3'
 
 #############################
 ##### Define Components #####
@@ -99,8 +85,6 @@ for idx, component in enumerate(components[1:], start=1):
     nfac_sys += numerator/denominator
 
 nfig_sys = rat_to_dB(nfac_sys) # Noise Figure of the whole system.
-print('\nCALCULATED SYSTEM PROPERTIES')
-print(f'System Noise Figure: {nfig_sys:.2f} dB')
 
 #############################
 ##### Total System Gain #####
@@ -110,7 +94,6 @@ print(f'System Noise Figure: {nfig_sys:.2f} dB')
 gain_dB_sys = 0
 for component in components:
     gain_dB_sys += component.gain_dB
-print(f'Total System Gain: {gain_dB_sys:.2f} dB')
 
 ################################
 ##### Output SNR and Power #####
@@ -119,12 +102,10 @@ print(f'Total System Gain: {gain_dB_sys:.2f} dB')
 # Find the output SNR.
 snr_in = PIN - pnoise_in # Input SNR in dB.
 snr_out = snr_in - nfig_sys # Output SNR in dB.
-print(f'Output SNR: {snr_out:.2f} dB')
 
 # Find the output power and peak to peak voltage at the output.
 pout = PIN + gain_dB_sys # The output power in dBm.
 Vpp_out = 2*dBm_to_Vpk(pout)
-print(f'Vpp at Output: {Vpp_out*1e3:.2f} mV')
 
 ########################
 ##### IM3 Products #####
@@ -139,12 +120,10 @@ for idx, component in enumerate(components[1:], start=1):
     running_sum += 1/new_term
 iip3_sys_W = 1/running_sum
 iip3_sys_dBm = rat_to_dB(iip3_sys_W/1e-3) # The system IIP3 in dBm.
-print(f'System IIP3: {iip3_sys_dBm:.2f} dBm')
 
 # Find the input power at which IM3 products become problematic (this is step 4 in the example).
 # There will be severe IM3 desense when two tones having this power are applied to the input.
 p_im3 = (POUT_WANTED - gain_dB_sys + 2*iip3_sys_dBm) / 3
-print(f'Severe IM3 Desense: {p_im3:.2f} dBm')
 
 ######################
 ##### ADC Sizing #####
@@ -156,7 +135,6 @@ Pfs = Vpk_to_dBm(1 / 2) # The argument to this function is the voltage amplitude
 Pq = noise_adc_in - 10
 snr_adc = Pfs - Pq
 enob = (snr_adc-1.76) / 6.02 # Estimated number of bits.
-print(f'Required ADC bits: {enob:.1f} bits')
 
 ##########################
 ##### BER Estimation #####
@@ -164,28 +142,109 @@ print(f'Required ADC bits: {enob:.1f} bits')
 
 # I already calculated the output SNR, so I do not need to do that again.
 ber = 0.5 * np.exp(-snr_out / 2)
-print(f'Estimated BER: {ber:.2e} (unitless)\n')
 
-############################################
-##### Power and Voltage for Each Block #####
-############################################
+##############################################
+##### Calculated System Properties Table #####
+##############################################
 
+# Make a table from the calculated system properties.
+data_dict = {
+    'Metric': [
+        'System Noise Figure', 'Total System Gain', 'Output SNR', 
+        'Vpp at Output', 'System IIP3', 'Severe IM3 Desense', 
+        'Required ADC Bits', 'Estimated BER'
+    ],
+    'Value': [
+        f'{nfig_sys:.2f} dB', f'{gain_dB_sys:.2f} dB', f'{snr_out:.2f} dB',
+        display_voltage(Vpp_out), f'{iip3_sys_dBm:.2f} dBm', 
+        f'{p_im3:.2f} dBm', f'{enob:.2f} bits', f'{ber:.2e} (unitless)'
+    ]
+}
+
+df = pd.DataFrame(data=data_dict)
+fig, ax = plt.subplots(figsize=(4,2))
+ax.axis('off')
+ax.axis('tight')
+table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
+plt.savefig(os.path.join(TABLE_DIR, 'calculated_sys_properties.png'), dpi=300)
+
+###################################
+##### Power and Voltage Table #####
+###################################
+
+# Initalize the variables used in this table.
+points = ['A', 'B', 'C', 'D', 'E', 'F']
+signal_powers = list()
+noise_powers = list()
+signal_amplitudes = list()
+snr_values = list()
+
+# Fill in the data for this table.
 current_sig_pwr = PIN
 current_noise_pwr = pnoise_in
 current_Vpk = dBm_to_Vpk(PIN)
-print('POWER AND VOLTAGE FOR EACH COMPONENT')
-
 for component in components:
 
-    print(f'Component Name: {component.name}')
-    print(f'\tInput Signal Power: {current_sig_pwr:.2f} dBm')
-    print(f'\tInput Noise Power: {current_noise_pwr:.2f} dBm')
-    print(f'\tInput Signal Vpk: {display_voltage(current_Vpk)}')
+    signal_powers.append(f'{current_sig_pwr:.1f}')
+    noise_powers.append(f'{current_noise_pwr:.0f}')
+    signal_amplitudes.append(display_voltage(current_Vpk))
+    snr_values.append(f'{current_sig_pwr - current_noise_pwr:.1f}')
 
     current_sig_pwr += component.gain_dB
     current_noise_pwr += component.gain_dB + component.nfig
     current_Vpk = dBm_to_Vpk(current_sig_pwr)
 
-    print(f'\tOutput Signal Power: {current_sig_pwr:.2f} dBm')
-    print(f'\tOutput Noise Power: {current_noise_pwr:.2f} dBm')
-    print(f'\tOutput Signal Vpk: {display_voltage(current_Vpk)}')
+# Consolidate the data into a single dictionary.
+data_dict = {
+    'Point': points,
+    'Signal Power (dBm)': signal_powers,
+    'Noise Power (dBm)': noise_powers,
+    'SNR (dB)': snr_values,
+    'Signal Amplitude': signal_amplitudes
+}
+
+# Create and save the table.
+df = pd.DataFrame(data=data_dict)
+fig, ax = plt.subplots(figsize=(8,2))
+ax.axis('off')
+ax.axis('tight')
+table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
+table.auto_set_font_size(False)
+table.set_fontsize(10)
+plt.savefig(os.path.join(TABLE_DIR, 'sig_pwr_voltages.png'), dpi=300)
+
+
+##############################
+##### System Input Table #####
+##############################
+
+# Build the dictionary to fill in this table.
+names = list()
+gain_vals = list()
+nfig_vals = list()
+iip3_vals = list()
+cpout_vals = list()
+for component in components:
+    names.append(component.name)
+    gain_vals.append(component.gain_dB)
+    nfig_vals.append(component.nfig)
+    iip3_vals.append(component.iip3_dBm)
+    cpout_vals.append(component.cpout)
+
+data_dict = {
+    'Block Name': names,
+    'Gain (dB)': gain_vals,
+    'Noise Figure (dB)': nfig_vals,
+    'IIP3 (dBm)': iip3_vals,
+    'Output Comp (dBm)': cpout_vals
+}
+
+# Create the table from that dictinoary.
+df = pd.DataFrame(data=data_dict)
+fig, ax = plt.subplots(figsize=(7,2))
+ax.axis('off')
+ax.axis('tight')
+table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
+table.auto_set_font_size(False)
+table.set_fontsize(8)
+plt.savefig(os.path.join(TABLE_DIR, 'sys_inputs.png'), dpi=300)
