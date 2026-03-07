293cc3f7a4118140264640780053e9a1f0f3b341
swalla27 <swalla27@asu.edu>
2026-03-07 11:27:35 -0600

    added iip2 and out cp

--- labs/lab3/component_class.py
+++ labs/lab3/component_class.py
@@ -14,7 +14,7 @@ import sys
 import os
 
 sys.path.append('/home/steven-wallace/Documents/asu/eee-598')
-from unit_conv_func import *
+from unit_conv_func import dB_to_rat, rat_to_dB, Vpk_to_dBm, dBm_to_Vpk
 
 plt.rcParams.update({
     "font.family": "serif",
@@ -35,18 +35,21 @@ def display_voltage(voltage: float):
         return f'{voltage:.1f} V' 
 
 class Component():
-    def __init__(self, name: str, gain_dB: float, nfig: float, iip3_dBm: float, cpout: float):
+    def __init__(self, name: str, gain_dB: float, nfig: float, iip3_dBm: float, iip2_dBm: float, cpout_dBm: float):
         self.name = name # A string describing what this component is.
         self.gain_dB = gain_dB # Gain in dB.
         self.nfig = nfig # Noise Figure, which has units of dB.
         self.iip3_dBm = iip3_dBm # Component IIP3 in dBm.
-        self.cpout = cpout # Output 1dB compression point.
+        self.iip2_dBm = iip2_dBm # Component IIP2 in dBm.
+        self.cpout_dBm = cpout_dBm # Output 1dB compression point.
 
-        self.cpin = (cpout+1) - gain_dB # Input 1dB compression point.
+        self.cpin_dBm = (cpout_dBm+1) - gain_dB # Input 1dB compression point.
         self.nfac = dB_to_rat(self.nfig) # Noise Factor, which is unitless.
         self.gain_rat = dB_to_rat(self.gain_dB) # Gain as a ratio.
         self.iip3_W = 1e-3 * dB_to_rat(self.iip3_dBm) # IIP3 in units of W.
-        self.dyn_range = self.cpin - PIN # Dynamic range of this component.
+        self.iip2_W = 1e-3 * dB_to_rat(self.iip2_dBm) # IIP2 in units of W.
+        self.dyn_range = self.cpin_dBm - PIN # Dynamic range of this component.
+        self.cpout_W = 1e-3 * dB_to_rat(self.cpout_dBm) # The output compression point in units of W.
 
 #####################
 ##### Constants #####
@@ -65,22 +68,22 @@ TABLE_DIR = '/home/steven-wallace/Documents/asu/eee-598/labs/lab3'
 # Define all of the components in the signal chain.
 # My values.
 components = [
-    Component(name='Switch', gain_dB=-2.0, nfig=0.8, iip3_dBm=30, cpout=20),
-    Component(name='Front BPF', gain_dB=-1.5, nfig=1.0, iip3_dBm=30, cpout=20),
-    Component(name='LNA', gain_dB=10, nfig=2, iip3_dBm=-5, cpout=5),
-    Component(name='Mixer', gain_dB=-6, nfig=6, iip3_dBm=5, cpout=10),
-    Component(name='BB Amp', gain_dB=60, nfig=6, iip3_dBm=10, cpout=15),
-    Component(name='ADC Filter', gain_dB=-2.0, nfig=1, iip3_dBm=30, cpout=20)
+    Component(name='Switch', gain_dB=-2.0, nfig=0.8, iip3_dBm=30, iip2_dBm=35, cpout_dBm=20),
+    Component(name='Front BPF', gain_dB=-1.5, nfig=1.0, iip3_dBm=30, iip2_dBm=35, cpout_dBm=20),
+    Component(name='LNA', gain_dB=10, nfig=2, iip3_dBm=-5, iip2_dBm=0, cpout_dBm=5),
+    Component(name='Mixer', gain_dB=-6, nfig=6, iip3_dBm=5, iip2_dBm=10, cpout_dBm=10),
+    Component(name='BB Amp', gain_dB=60, nfig=6, iip3_dBm=10, iip2_dBm=15, cpout_dBm=15),
+    Component(name='ADC Filter', gain_dB=-2.0, nfig=1, iip3_dBm=30, iip2_dBm=35, cpout_dBm=20)
 ]
 
 # The values from the example.
 # components = [
-#     Component(name='Switch', gain_dB=-0.8, nfig=0.8, iip3_dBm=30, cpout=20),
-#     Component(name='Front BPF', gain_dB=-1.0, nfig=1.0, iip3_dBm=30, cpout=20),
-#     Component(name='LNA', gain_dB=15, nfig=2, iip3_dBm=-5, cpout=5),
-#     Component(name='Mixer', gain_dB=-6, nfig=6, iip3_dBm=5, cpout=10),
-#     Component(name='BB Amp', gain_dB=30, nfig=6, iip3_dBm=10, cpout=15),
-#     Component(name='ADC Filter', gain_dB=-1, nfig=1, iip3_dBm=30, cpout=20)
+#     Component(name='Switch', gain_dB=-0.8, nfig=0.8, iip3_dBm=30, cpout_dBm=20),
+#     Component(name='Front BPF', gain_dB=-1.0, nfig=1.0, iip3_dBm=30, cpout_dBm=20),
+#     Component(name='LNA', gain_dB=15, nfig=2, iip3_dBm=-5, cpout_dBm=5),
+#     Component(name='Mixer', gain_dB=-6, nfig=6, iip3_dBm=5, cpout_dBm=10),
+#     Component(name='BB Amp', gain_dB=30, nfig=6, iip3_dBm=10, cpout_dBm=15),
+#     Component(name='ADC Filter', gain_dB=-1, nfig=1, iip3_dBm=30, cpout_dBm=20)
 # ]
 
 ###############################
@@ -88,6 +91,8 @@ components = [
 ###############################
 
 # Find the noise factor of the system.
+# This implements the equation found here:
+# https://en.wikipedia.org/wiki/Friis_formulas_for_noise
 nfac_sys = components[0].nfac
 denominator = 1
 for idx, component in enumerate(components[1:], start=1):
@@ -118,11 +123,13 @@ snr_out = snr_in - nfig_sys # Output SNR in dB.
 pout = PIN + gain_dB_sys # The output power in dBm.
 Vpp_out = 2*dBm_to_Vpk(pout)
 
-########################
-##### IM3 Products #####
-########################
+#########################
+##### Cascaded IIP3 #####
+#########################
 
 # This section will find the IIP3 of the entire system.
+# It implements the equation found here:
+# https://www.rfcafe.com/references/electrical/ip3.htm
 running_sum = 1 / components[0].iip3_W
 gain_term = 1
 for idx, component in enumerate(components[1:], start=1):
@@ -154,21 +161,55 @@ enob = (snr_adc-1.76) / 6.02 # Estimated number of bits.
 # I already calculated the output SNR, so I do not need to do that again.
 ber = 0.5 * np.exp(-snr_out / 2)
 
+#########################
+##### Cascaded IIP2 #####
+#########################
+
+# This section will find the IIP2 of the entire system.
+# It implements the equation found here:
+# https://www.rfcafe.com/references/electrical/ip2.htm
+running_sum = 1 / np.sqrt(components[0].iip2_W)
+gain_term = 1
+for idx, component in enumerate(components[1:], start=1):
+    gain_term *= components[idx-1].gain_rat
+    new_term = np.sqrt(component.iip2_W * gain_term)
+    running_sum += 1/new_term
+iip2_sys_W = (1/running_sum)**2
+iip2_sys_dBm = rat_to_dB(iip2_sys_W/1e-3) # The system IIP2 in dBm.
+
+######################################
+##### Cascaded Compression Point #####
+######################################
+
+# This section will find the compression point of the entire system.
+# It implements the equation found here:
+# https://www.rfcafe.com/references/electrical/p1db.htm
+running_sum = 1 / components[0].cpout_W
+gain_term = 1
+for idx, component in enumerate(components[1:], start=1):
+    gain_term *= components[idx-1].gain_rat
+    new_term = component.cpout_W * gain_term
+    running_sum += 1/new_term
+cpout_sys_W = (1/running_sum)**2
+cpout_sys_dBm = rat_to_dB(cpout_sys_W/1e-3) # The system compression point in dBm.
+
 ##############################################
 ##### Calculated System Properties Table #####
 ##############################################
 
 # Make a table from the calculated system properties.
 data_dict = {
+    'Index': np.arange(1, 10+1),
     'Metric': [
         'System Noise Figure', 'Total System Gain', 'Output SNR', 
         'Vpp at Output', 'System IIP3', 'Severe IM3 Desense', 
-        'Required ADC Bits', 'Estimated BER'
+        'Required ADC Bits', 'Estimated BER', 'System IIP2', 'System Out CP'
     ],
     'Value': [
         f'{nfig_sys:.2f} dB', f'{gain_dB_sys:.2f} dB', f'{snr_out:.2f} dB',
         display_voltage(Vpp_out), f'{iip3_sys_dBm:.2f} dBm', 
-        f'{p_im3:.2f} dBm', f'{enob:.2f} bits', f'{ber:.2e} (unitless)'
+        f'{p_im3:.2f} dBm', f'{enob:.2f} bits', f'{ber:.2e} (unitless)',
+        f'{iip2_sys_dBm:.2f} dBm', f'{cpout_sys_dBm:.2f} dBm'
     ]
 }
 
@@ -179,11 +220,11 @@ ax.axis('tight')
 table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
 table.auto_set_font_size(False)
 table.set_fontsize(8)
-plt.savefig(os.path.join(TABLE_DIR, 'calculated_sys_properties.png'), dpi=300)
+plt.savefig(os.path.join(TABLE_DIR, 'cascaded_properties.png'), dpi=300, bbox_inches='tight')
 
-###################################
-##### Power and Voltage Table #####
-###################################
+##############################
+##### Signal Power Table #####
+##############################
 
 # Initalize the variables used in this table.
 points = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
@@ -224,31 +265,33 @@ data_dict = {
 
 # Create and save the table.
 df = pd.DataFrame(data=data_dict)
-fig, ax = plt.subplots(figsize=(8,2))
+fig, ax = plt.subplots(figsize=(8,1.5))
 ax.axis('off')
 ax.axis('tight')
 table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
 table.auto_set_font_size(False)
 table.set_fontsize(8)
-plt.savefig(os.path.join(TABLE_DIR, 'sig_pwr_voltages.png'), dpi=300)
+plt.savefig(os.path.join(TABLE_DIR, 'signal_powers.png'), dpi=300, bbox_inches='tight')
 
-##############################
-##### System Input Table #####
-##############################
+##########################################
+##### Component Specifications Table #####
+##########################################
 
 # Build the dictionary to fill in this table.
 names = list()
 gain_vals = list()
 nfig_vals = list()
 iip3_vals = list()
-cpout_vals = list()
+iip2_vals = list()
+cpout_dBm_vals = list()
 dyn_ranges = list()
 for component in components:
     names.append(component.name)
     gain_vals.append(component.gain_dB)
     nfig_vals.append(component.nfig)
     iip3_vals.append(component.iip3_dBm)
-    cpout_vals.append(component.cpout)
+    iip2_vals.append(component.iip2_dBm)
+    cpout_dBm_vals.append(component.cpout_dBm)
     dyn_ranges.append(component.dyn_range)
 
 data_dict = {
@@ -256,16 +299,17 @@ data_dict = {
     'Gain (dB)': gain_vals,
     'Noise Figure (dB)': nfig_vals,
     'IIP3 (dBm)': iip3_vals,
-    'Output Comp (dBm)': cpout_vals,
+    'IIP2 (dBm)': iip2_vals,
+    'Output Comp (dBm)': cpout_dBm_vals,
     'Dynamic Range (dB)': dyn_ranges
 }
 
 # Create the table from that dictinoary.
 df = pd.DataFrame(data=data_dict)
-fig, ax = plt.subplots(figsize=(8,2))
+fig, ax = plt.subplots(figsize=(9,1.5))
 ax.axis('off')
 ax.axis('tight')
 table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
 table.auto_set_font_size(False)
 table.set_fontsize(8)
-plt.savefig(os.path.join(TABLE_DIR, 'sys_inputs.png'), dpi=300)
+plt.savefig(os.path.join(TABLE_DIR, 'component_specs.png'), dpi=300, bbox_inches='tight')
